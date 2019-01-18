905b94629395634ce9b693210ff679428df41290
Andrei Ciornei <andrei.ciornei@outlook.com>
2019-01-18 06:15:36 +0200

    email service

---
+++ EmailService/EmailService/Controllers/EmailSenderController.cs
@@ -0,0 +1,33 @@
+﻿using EmailService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SendServiceApi;
+
+namespace EmailService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailSenderController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSenderController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        [HttpPost]
+        [Route("/api/v1/sendEmail")]
+        public void SendEmail([FromBody]EmailModel emailModel)
+        {
+            string url = _configuration["Email:EmailSendServiceBaseURL"];
+            string username = _configuration["Email:EmailSendServiceUsername"];
+            string password = _configuration["Email:EmailSendServicePassword"];
+
+            var service = new SendService(username, password, url);
+
+            var status = service.SendEmail("andrei.ciornei@outlook.com", "CloudXdb", emailModel.EmailTo,emailModel.Subject,emailModel.Content);
+        }
+    }
+}
---
+++ EmailService/EmailService/Models/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailService.Models
+{
+    public class EmailModel
+    {
+        public string EmailTo { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
